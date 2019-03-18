8a29fbac81bac12e99bfc34fd1c56e1085e66325
MilkGames <memegames1337@gmail.com>
2019-03-18 17:51:15 +0100

    Change syntax, see wiki, upload progress added

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
 "use strict";
 var request = require('request');
+var events = require('events').EventEmitter;
+var fs = require('fs');
 exports.fetchAPIKey = function(email, password) {
-  return new Promise((resolve, reject) => {
-    var req = request.post('https://lithi.io/api/v1/fetch-api-key.php', function(error, response, body) {
-      if (error) throw new Error(error);
-      if (JSON.parse(body).apikey) {
-        resolve(JSON.parse(body).apikey);
-      } else {
-        reject(JSON.parse(body).error);
-      }
-    });
-    var form = req.form();
-    form.append('email', email);
-    form.append('password', password);
-  })
-}
+  let Emitter = new events;
+  let req = request.post('https://lithi.io/api/v1/fetch-api-key.php', function(error, response, body) {
+    if (error) throw new Error(error);
+    if (JSON.parse(body).apikey) {
+      Emitter.emit('success', JSON.parse(body).apikey);
+    } else {
+      Emitter.emit('error', JSON.parse(body).error);
+    }
+  });
+  let form = req.form();
+  form.append('email', email);
+  form.append('password', password);
+  return Emitter;
+};
+
 exports.upload = function(apikey, data) {
-  return new Promise((resolve, reject) => {
-    var req = request.post('https://upload.lithi.io/v1.php', function(error, response, body) {
-      if (error) throw new Error(error);
-      if (JSON.parse(body).url) {
-      	resolve(JSON.parse(body).url);
-      } else {
-      	reject(JSON.parse(body).error);
-      }
-    })
-    var form = req.form();
-    form.append('key', apikey);
-    form.append('file', data);
-  })
-}
+  let Emitter = new events;
+  let req = request.post('https://upload.lithi.io/v1.php', function(error, response, body) {
+    if (error) throw new Error(error);
+    if (JSON.parse(body).url) {
+      Emitter.emit('success', JSON.parse(body).url);
+    } else {
+      Emitter.emit('error', JSON.parse(body).error);
+    }
+  }).on('drain', () => {
+    Emitter.emit('progress', req.req.connection._bytesDispatched*100/req.headers['content-length']);  
+  });
+  let form = req.form();
+  form.append('key', apikey);
+  form.append('file', data);
+  return Emitter;
+};
