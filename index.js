a965bbb863af446ba996d7bf4cbe86d0db2bdb3d
MilkGames <memegames1337@gmail.com>
2020-04-05 20:29:43 +0200

    fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var events = require('events').EventEmitter;
 var fs = require('fs');
 exports.fetchAPIKey = function(email, password) {
   let Emitter = new events;
-  let req = request.post('https://lithi.io/api/v1/fetch-api-key.php', function(error, response, body) {
+  let req = request.post('https://lithi.io/api/v2/fetch-api-key', function(error, response, body) {
     if (error) throw new Error(error);
     if (JSON.parse(body).apikey) {
       Emitter.emit('success', JSON.parse(body).apikey);
@@ -20,7 +20,7 @@ exports.fetchAPIKey = function(email, password) {
 
 exports.upload = function(apikey, data) {
   let Emitter = new events;
-  let req = request.post('https://upload.lithi.io/v1.php', function(error, response, body) {
+  let req = request.post('https://lithi.io/api/v2/upload', function(error, response, body) {
     if (error) throw new Error(error);
     if (JSON.parse(body).url) {
       Emitter.emit('success', JSON.parse(body).url);
@@ -31,7 +31,7 @@ exports.upload = function(apikey, data) {
     Emitter.emit('progress', req.req.connection._bytesDispatched*100/req.headers['content-length']);  
   });
   let form = req.form();
-  form.append('key', apikey);
+  form.append('api_key', apikey);
   form.append('file', data);
   return Emitter;
 }
